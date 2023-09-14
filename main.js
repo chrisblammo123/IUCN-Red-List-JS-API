f1df82ddbb98160102cfbd2ce32e618d7c88da2d
Chris Struck <chris.struck@cstruck.dev>
2023-09-14 18:55:49 -0500

    added some tests, got it working after a few dumb issues

--- main.js
+++ main.js
@@ -1,17 +1,51 @@
 
 // External Node Modules
-const http = require('');
+
+const axios = require('axios');					// HTTP Client for Node.js
 
 
-// Get files in
-const config = require('./config.json');
+// Local Files
 
+const config = require('./config.json');		// Mostly just for the API token
 
 
 // Variables
-const base = 'https://apiv3.iucnredlist.org/api/v3/'
-var query = ''
-var parameter = ''			// takes in parameter optional var from func converts to "/:xxx" format 
-var requiresToken = true			// All but at most four require the token
+
+// presumably not needed with axios over other node modules
+// const base = 'https://apiv3.iucnredlist.org/api/v3/'
+// var query = ''
+// var parameter = ''			// takes in parameter optional var from func converts to "/:xxx" format 
+// var requiresToken = true			// All but at most four require the token
 // will probably have something like: requiresToken = query.containedinlist(big list of possible queries)
-var endpoint = base + query + parameter + (requiresToken ? '?token=' + config.token : '')
+// var endpoint = base + query + parameter + (requiresToken ? '?token=' + config.token : '')
+
+
+
+module.exports = class IUCNRedList {
+	constructor(apiKey) {
+		axios.defaults.params = {}
+		axios.defaults.baseURL = 'https://apiv3.iucnredlist.org/api/v3/'
+		axios.defaults.params['token'] = apiKey
+		axios.defaults.geaders
+		// axios.defaults.headers.common['Authorization'] = `Bearer ${apiKey}`;		// ignore for now
+		// Yeah the info I saw was unreliable but it should work now
+	}
+
+	// async proMatches() {
+	// 	const { data } = await axios.get(`${this.baseUrl}/proMatches`);
+	// 	return data;
+	// }
+
+	async version() {
+		const { data } = await axios.get('/version', {'token': null});
+		return data;
+	}
+
+	// TODO: Change to class later
+	async countries() {
+		const { data } = await axios.get('/country/list');
+		return data;
+	}
+}
+
+
---
+++ test.js
@@ -0,0 +1,24 @@
+// Imports
+
+const IUCNRedList = require('./main.js');			// Main API Wrapper
+const config = require('./config.json');			// Config / API Tokens
+
+// Sets the API Key to an empty string if undefined
+const API_KEY = typeof config.testtoken != 'undefined' ? config.testtoken : '';
+
+// Creates the Wrapper API Class
+const icun = new IUCNRedList(API_KEY);
+
+// TODO: Add a couple more tests here
+(async () => {
+	console.log('Checking IUCN Red List API Version:\n...');
+	console.log(await icun.version());
+
+	console.log();
+
+	console.log('Querying IUCN Red List API for a List of Countries:\n...');
+	console.log(await icun.countries().catch( (err) => {
+		console.error(err);
+	}));
+
+})();
